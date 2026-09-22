01088f9988718263ff61b53d661035f67b47c75c
EL-CONS <gabrielcons117@live.com>
2026-09-22 12:44:21 -0700

    fixed pixel ratios bc toc

--- script.js
+++ script.js
@@ -695,7 +695,7 @@ function rebootOS() {
 
 
 applyTheme(document.querySelector('input[name="theme"]:checked')?.value || 'gray-lcd');
-playBootAnimation(4000);
+playBootAnimation(40);
 
 
 //****************************** */ 
